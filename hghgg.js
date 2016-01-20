0e4d756e0a2fc2e0a98220a49d3d362546b0cf98
ProEbram <Pro@ebram.comlu.com>
2016-01-20 23:39:46 +0200

    Create hghgg.js

---
+++ hghgg.js
@@ -0,0 +1,42 @@
+<style>
+@import url(http://fonts.googleapis.com/earlyaccess/notosanskufiarabic.css);
+#subscribebox{background:#576269;padding:20px;font-family:&#39;PT Sans&#39;,sans-serif;}
+.widget_follow_subscribe .widget-detail{padding:36px 30px 40px}
+#subscribebox p{color:#fff;font-size:15px;text-align:center;font-weight:700;font-family: 'Noto Sans Kufi Arabic', sans-serif!important; line-height: 3em;}
+.follow-subscribe-social{padding:0 0 14px;border-bottom:#858585 solid 1px}
+.follow-subscribe-social ul{list-style:none;margin:0;padding:0;text-align:center}
+.follow-subscribe-social ul li{display:inline;margin-left: 20px;border-bottom:none}
+.follow-subscribe-social ul li:last-child{margin:0}
+.follow-subscribe-social ul li a{font-size:17px;color:#cacaca;-webkit-transition:color .2s ease-in-out;-moz-transition:color .2s ease-in-out;-ms-transition:color .2s ease-in-out;-o-transition:color .2s ease-in-out;transition:color .2s ease-in-out}
+.follow-subscribe-social ul li a:hover{color:#fff}
+form.subscribe{margin-top:-7px}
+form.subscribe input{display:block;width:100%}
+.subscribe-email{height:45px;border:none;margin:0 0 10px;font-size:.928571em;background-color:rgba(255,255,255,0.2);text-align:center;color:#fff}
+.subscribe-email:focus{outline:0}
+form.subscribe .placeholder{color:#cacaca}
+form.subscribe input:-ms-input-placeholder{color:#cacaca}
+form.subscribe input::-webkit-input-placeholder{color:#cacaca}
+form.subscribe input:-moz-placeholder{color:#fafafa}
+form.subscribe input::-moz-placeholder{color:#fafafa}
+.subscribe-button{height: 45px; font-weight: 700; font-size: 16px; color: #fff; text-transform: uppercase; border: none; background-color: #4FC4F6; transition: background-color .2s ease-in-out; font-family: 'Droid Arabic Naskh', serif;}
+.subscribe-button:hover{background-color:#00A3CE}
+.subscribe-button:focus{outline:0}
+</style>
+<div id="subscribebox">
+<div class="follow-subscribe-social">
+<ul>
+<li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#" target="_blank"><i class="fa fa-google"></i></a></li>
+<li><a href="#" target="_blank"><i class="fa fa-youtube"></i></a></li>
+<li><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+</ul>
+</div>
+<p>اشترك في النشرة البريدية</p>
+<form class="subscribe" action='http://feedburner.google.com/fb/a/mailverify?uri=ar1web/TFDh' method='post' onsubmit='window.open(&apos;http://feedburner.google.com/fb/a/mailverify?uri=ar1web/TFDh, &apos;popupwindow&apos;, &apos;scrollbars=yes,width=550,height=520&apos;);return true' target='popupwindow'>
+<input name='uri' type='hidden' value='اسمك المستخدم'/>
+<input name='loc' type='hidden' value='en_US'/>
+<input class="subscribe-email" type='text' name='email' onblur='if (this.value == &quot;&quot;) {this.value = &quot;Your Email&quot;;}' onfocus='if (this.value == &quot;Your Email&quot;) {this.value = &quot;&quot;;}' value='بريدك الالكتروني'/>
+<input class="subscribe-button" type="submit" value="اشترك الان" />
+</form>
+</div>
